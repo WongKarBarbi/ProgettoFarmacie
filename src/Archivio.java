aae4f3701590edde17ad10cc6d8531f3f07fbc02
WongKarBarbi <francesco.barbiero@itiszuccante.edu.it>
2025-04-01 12:50:19 +0200

    Primo commit

---
+++ src/Archivio.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+
+public class Archivio {
+
+    private ArrayList <Farmacia> Farmacie = new ArrayList<>();
+
+
+
+    //metoodo di importo
+    public void importaDati(String fileName){
+
+    }
+
+
+    //metodo di filtraggio
+    public void filtraIndirizzo(){}
+
+    public void filtraNome(){}
+
+    public void filtraCittà(){}
+
+
+
+    //metodo di ricerca
+    public void ricercaIndirizzo(){}
+
+    public void ricercaNome(){}
+
+    public void ricercaCittà(){}
+
+}
---
+++ src/Farmacia.java
@@ -0,0 +1,32 @@
+public class Farmacia {
+    private String nome;
+    private String indirizzo;
+    private String città;
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getIndirizzo() {
+        return indirizzo;
+    }
+
+    public void setIndirizzo(String indirizzo) {
+        this.indirizzo = indirizzo;
+    }
+
+    public String getCittà() {
+        return città;
+    }
+
+    public void setCittà(String città) {
+        this.città = città;
+    }
+
+
+}
